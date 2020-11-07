96a63dcb75cdf7fc99e6cb587e829c771c3836b9
lwy-lenovo <527563266@qq.com>
2020-11-07 23:58:06 +0800

    add todo list test

---
+++ todo-list-test/src/components/Todo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  render() {
+    const { text, completed, onClick } = this.props;
+    return (
+      <li 
+        onClick={onClick}
+      style={{
+        textDecoration: completed ? 'line-through' : 'none'
+      }}>
+        { text }
+      </li>
+    );
+  }
+}
+
+export default Todo;
