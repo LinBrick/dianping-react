c980497ea03b83b9b33bd9973423fc4aac594248
lwy-lenovo <527563266@qq.com>
2020-11-21 16:49:56 +0800

    实现删除订单功能

--- src/containers/User/components/OrderItem/index.js
+++ src/containers/User/components/OrderItem/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./style.css"
+import "./style.css";
 
 class OrderItem extends Component {
   render() {
@@ -25,17 +25,41 @@ class OrderItem extends Component {
           <div className="orderItem__type">{channel}</div>
           <div>
             {type === 1 ? <div className="orderItem__btn">评价</div> : null}
-            <div className="orderItem__btn" onClick={this.handleRemove}>删除</div>
+            <div className="orderItem__btn" onClick={this.handleRemove}>
+              删除
+            </div>
           </div>
         </div>
       </div>
     );
   }
 
+  renderStars() {
+    return (
+      <div>
+        {[1, 2, 3, 4, 5].map((item, index) => {
+          const lightClass = 3 >= item ? "orderItem__star--light" : "";
+          return (
+            <span
+              className={"orderItem__star " + lightClass}
+              key={index}
+              onClick={null}
+            >
+              ★
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
+
+  //评价信息发生变化
+  handleCommentChange = () => {};
+
   //删除订单
   handleRemove = () => {
     this.props.onRemove();
-  }
+  };
 }
 
 export default OrderItem;
